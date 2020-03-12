f26a453622db63499802e81e6eb8b20ecea197fe
JAPM <cicelcup@gmail.com>
2020-03-12 12:11:35 -0600

    add menu with setting fragment

--- app/src/main/java/com/example/testing_navigation/MainActivity.kt
+++ app/src/main/java/com/example/testing_navigation/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.testing_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
     }
 }
