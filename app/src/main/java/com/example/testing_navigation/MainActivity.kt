04e21af0448204c9b3937537d03ee8a0cd5c9b91
JAPM <cicelcup@gmail.com>
2020-03-12 14:58:56 -0600

    add Toolbar

--- app/src/main/java/com/example/testing_navigation/MainActivity.kt
+++ app/src/main/java/com/example/testing_navigation/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.onNavDestinationSelected
@@ -20,14 +22,27 @@ class MainActivity : AppCompatActivity() {
         val host = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        //Bottom Navigation
+        //NavigationController
+        val navigationController = host.navController
+
+        setToolBar()
+
+        setBottomMenu(navigationController)
+    }
+
+    private fun setToolBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun setBottomMenu(navigationController: NavController) {
         val bottomMenu =
             findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomMenu.setupWithNavController(host.navController)
+        bottomMenu.setupWithNavController(navigationController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.navigation_menu,menu)
+        menuInflater.inflate(R.menu.top_right_menu,menu)
         return true
     }
 
