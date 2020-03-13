d3d0b5459b5ac4d5ed555f5cdb1939213fbcc732
JAPM <cicelcup@gmail.com>
2020-03-13 17:19:01 -0600

    add deep link

--- app/src/main/java/com/example/testing_navigation/MusicFragment.kt
+++ app/src/main/java/com/example/testing_navigation/MusicFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +18,12 @@ class MusicFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_music, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textViewMusic2)
+            .text = arguments?.let { MusicFragmentArgs.fromBundle(it).arg }
     }
 
 }
